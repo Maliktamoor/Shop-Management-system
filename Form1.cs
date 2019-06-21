09527912d46d9159b918ef71818fb41053fdb1ba
abdullah707 <52041575+abdullah707@users.noreply.github.com>
2019-06-21 14:26:37 +0500

    abdullah
    i work about supplier

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace ShopManagement
+{ 
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataContext dc=new DataContext("Data Source=DESKTOP-21TJ386\\SQLEXPRESS;Initial Catalog=shop;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Table<supplier> s1= dc.GetTable<supplier>();
+            supplier s= new supplier();
+            s._ID= Convert.ToInt32(textBox1.Text);
+            s._name= textBox2.Text;
+            s._age = Convert.ToInt32(textBox3.Text);
+            s._address = textBox4.Text;
+            s._contact = textBox5.Text;
+            s1.InsertOnSubmit(s);
+            dc.SubmitChanges();
+            MessageBox.Show("Data Added");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            View c = new View();
+            c.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ supplier.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace ShopManagement
+{
+   [Table(Name= "Supplier")]
+    class supplier
+    {
+        int ID;
+       [Column(Name= "Sup_ID",IsPrimaryKey=true)]
+        public int _ID 
+        {
+            get {return ID ;}
+            set {ID= value ;}
+        }
+        string name;
+       [Column(Name = "Sup_Name")]
+        public string _name 
+        {
+            get { return name; }
+            set {name= value ;}
+        }
+        int age;
+       [Column(Name = "Sup_Age")]
+        public int _age 
+        {
+            get {return age ;}
+            set {age= value ;}
+        }
+        string address;
+       [Column(Name = "Sup_Address")]
+        public string _address 
+        {
+            get {return address ;}
+            set {address= value ;}
+        }
+        string contact;
+       [Column(Name = "Sup_Contact")]
+        public string _contact 
+        {
+            get {return contact ;}
+            set {contact=value ;}
+        }
+
+    }
+}
